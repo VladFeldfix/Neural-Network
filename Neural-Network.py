917b81338a0432ce3ef9d11c44e033f32450523f
Vlad Feldfix <62237119+VladFeldfix@users.noreply.github.com>
2024-04-24 15:59:39 +0300

    Add files via upload

--- Neural-Network.py
+++ Neural-Network.py
@@ -20,13 +20,19 @@ class NeuralNetwork:
         self.nn.append([])
         for _ in outpus:
             self.nn[-1].append(random.uniform(0, 1))
-        
+        self.outpus = outpus
         #print(self.nn)
 
     def get_labeled_data(self, data, lbl):
         result = data
         for layer in self.nn:
             result = self.matrix_multiplication(result, layer)
+        
+        # what is the result
+        #print(result)
+        #print(result.index(max(result)))
+        chosen_result_index = result.index(max(result))
+        print(self.outpus[chosen_result_index])
             
     def matrix_multiplication(self, A, B):
         #print(A)
@@ -38,52 +44,8 @@ class NeuralNetwork:
                 ans += a*b
             y = 1 / (1 + (math.e ** (-1 * ans)))
             result.append(y)
-        print(result)
+        #print(result)
         return result
+
 nn = NeuralNetwork(4,5,3,['square','circe'])
 nn.get_labeled_data([0.1,0.2,0.3],'square')
-"""
-class NeuralNetwork:
-    def __init__(self, inputs, hidden_layers_qty, hidden_layers_size, outpus):
-        self.nn = [[]]
-        # first hidden layer
-        for hls in range(hidden_layers_size):
-            self.nn[0].append([])
-            for _ in range(inputs):
-                self.nn[0][hls].append(0.5)
-        
-        # rest of hidden layers
-        for hlq in range(hidden_layers_qty):
-            self.nn.append([])
-            for hls in range(hidden_layers_size):
-                self.nn[hlq+1].append([])
-                for _ in range(hidden_layers_size):
-                    self.nn[hlq+1][hls].append(0.4)
-    
-    def get_labeled_data(self, labeled_data):
-        # labeled_data = [
-        #                   [[list, of, nodes, to, process, ...], lbl],
-        #                   [[list, of, nodes, to, process, ...], lbl],
-        #                   ...
-        #                ]
-        # go over each data node
-        for data in labeled_data:
-            nodes = data[0]
-            lbl = data[1]
-            #print(nodes)
-            # [0.254, 1.251, 2.251]
-            # [0.245, 1.214, 1.541]
-            self.matrix_multiplication(nodes,self.nn[0])
-    
-    def matrix_multiplication(self, A, B):
-        result = []
-        for bb in B:
-            for b in bb:
-                for a in A:
-                  result.append(a*b)
-        print(result)
-        
-nn = NeuralNetwork(4,5,3,['square','circe'])
-nn.get_labeled_data([[[0.1,0.2,0.3],'square'],[[0.4,0.5,0.6],'circe']])
-
-"""
