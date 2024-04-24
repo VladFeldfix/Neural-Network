da7d919312a54b65a0af351d0e314e92a87bef98
Vlad Feldfix <62237119+VladFeldfix@users.noreply.github.com>
2024-04-24 15:52:05 +0300

    Add files via upload

--- Neural-Network.py
+++ Neural-Network.py
@@ -1,33 +1,89 @@
+import math
+import random
+
 class NeuralNetwork:
-    def __init__(self, input_nodes_qty, hidden_layers_qty, hidden_layers_size, output_nodes):
-        # setup global vars
-        self.input_nodes_qty = input_nodes_qty          # number of input nodes min 2
-        self.hidden_layers_qty = hidden_layers_qty      # number of hidden layers min 0
-        self.hidden_layers_size = hidden_layers_size    # number of nodes in each hidden layer min 1 (if hidden_layers_qty == 0 then hidden_layers_size is ignored)
-        self.output_nodes_qty = output_nodes_qty        # list of output nodes labeled min 2
+    def __init__(self, inputs, hidden_layers_qty, hidden_layers_size, outpus):
+        self.nn = [[]]
+        # first hidden layer
+        for _ in range(inputs):
+            for _ in range(hidden_layers_size):
+                self.nn[0].append(random.uniform(0, 1))
 
-        # test input
-        if input_nodes_qty < 2:
-            raise ValueError("input_nodes_qty must be min 2")
-        if hidden_layers_qty < 0:
-            raise ValueError("hidden_layers_qty must be min 0")
-        if hidden_layers_size < 1:
-            raise ValueError("hidden_layers_size must be min 1")
-        if len(output_nodes_qty) < 2:
-            raise ValueError("output_nodes_qty must be min 2")
+        # rest of hidden layers
+        for hls in range(hidden_layers_qty):
+            self.nn.append([])
+            for _ in range(hidden_layers_size):
+                for _ in range(hidden_layers_size):
+                    self.nn[hls+1].append(random.uniform(0, 1))
 
-        # generate empty untrained neural network
-        nn = []
-        for hlq in range(self.hidden_layers_qty):
-            nn.append([])
-            for hls in range(self.hidden_layers_size):
-                nn[hlq].append(0.5)
-
-    def get_labeled_data(self, labeled_data, lbl):
-        # make sure the data size is correct
-        if len(labeled_data) != self.input_nodes_qty:
-            raise ValueError("labeled_data size must be a list of exactly: "+str(self.input_nodes_qty)+" nodes")
+        # results
+        self.nn.append([])
+        for _ in outpus:
+            self.nn[-1].append(random.uniform(0, 1))
         
-        # 
+        #print(self.nn)
 
-NeuralNetwork(3,4,4,4)
+    def get_labeled_data(self, data, lbl):
+        result = data
+        for layer in self.nn:
+            result = self.matrix_multiplication(result, layer)
+            
+    def matrix_multiplication(self, A, B):
+        #print(A)
+        #print(B)
+        result = []
+        for b in B:
+            ans = 0
+            for a in A:
+                ans += a*b
+            y = 1 / (1 + (math.e ** (-1 * ans)))
+            result.append(y)
+        print(result)
+        return result
+nn = NeuralNetwork(4,5,3,['square','circe'])
+nn.get_labeled_data([0.1,0.2,0.3],'square')
+"""
+class NeuralNetwork:
+    def __init__(self, inputs, hidden_layers_qty, hidden_layers_size, outpus):
+        self.nn = [[]]
+        # first hidden layer
+        for hls in range(hidden_layers_size):
+            self.nn[0].append([])
+            for _ in range(inputs):
+                self.nn[0][hls].append(0.5)
+        
+        # rest of hidden layers
+        for hlq in range(hidden_layers_qty):
+            self.nn.append([])
+            for hls in range(hidden_layers_size):
+                self.nn[hlq+1].append([])
+                for _ in range(hidden_layers_size):
+                    self.nn[hlq+1][hls].append(0.4)
+    
+    def get_labeled_data(self, labeled_data):
+        # labeled_data = [
+        #                   [[list, of, nodes, to, process, ...], lbl],
+        #                   [[list, of, nodes, to, process, ...], lbl],
+        #                   ...
+        #                ]
+        # go over each data node
+        for data in labeled_data:
+            nodes = data[0]
+            lbl = data[1]
+            #print(nodes)
+            # [0.254, 1.251, 2.251]
+            # [0.245, 1.214, 1.541]
+            self.matrix_multiplication(nodes,self.nn[0])
+    
+    def matrix_multiplication(self, A, B):
+        result = []
+        for bb in B:
+            for b in bb:
+                for a in A:
+                  result.append(a*b)
+        print(result)
+        
+nn = NeuralNetwork(4,5,3,['square','circe'])
+nn.get_labeled_data([[[0.1,0.2,0.3],'square'],[[0.4,0.5,0.6],'circe']])
+
+"""
